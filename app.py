651f56b34e58769ac055c05d7fdbe6f795dcafee
Valentine Svensson <v@nxn.se>
2015-08-16 23:58:59 +0100

    added everything to repo

---
+++ app.py
@@ -0,0 +1,51 @@
+import os
+import json
+
+from sklearn.externals import joblib
+from sklearn.base import TransformerMixin 
+from flask import Flask, Response, render_template
+
+class Reducer(object):
+    def __init__(self, idx):
+        self.idx = idx
+    
+    def fit_transform(self, X, y=None, **fit_params):
+        return self.transform(X)
+    
+    def transform(self, X):
+        return X[:, self.idx]
+    
+    def get_params(self, *k, **kw):
+        return {'': ''}
+
+name_clf = joblib.load('name_clf2.pkl')
+class_names = name_clf.get_params()['clf'].classes_
+
+app = Flask(__name__)
+
+
+@app.route('/guess/<name>')
+def guess(name='test'):
+    print name
+    log_proba = name_clf.predict_log_proba([name])
+    t5 = log_proba.argsort(axis=1)[:,-5:]
+    b = class_names[t5]
+    predictions = []
+    for c, lp in zip(b[0], log_proba[:, t5].flatten())[::-1]:
+        predictions.append({'country': c, 'probability': lp})
+
+#    predictions = []
+#    for c, lp in zip(['q', 'w', 'e', 'r', 't'], [0.1, 0.2, 0.3, 0.4, 0.5]):
+#        predictions.append({'country': c, 'probability': lp})
+
+    return Response(json.dumps(predictions, indent=2), mimetype='application/json')
+
+
+@app.route('/')
+@app.route('/<name>')
+def index(name='John Smith'):
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,37 @@
+(function() {
+	var name;
+
+	guess_country = function(url, callback) {
+		var req;
+		req = $.getJSON(url);
+		return req.success(function(data) {
+			return callback(data)
+		});
+	};
+
+	disp = function(data) {
+		$("#results h3").remove()
+		var results
+		results = $("#results")
+		for (_i = 0, _len = data.length; _i < _len; _i++) {
+			results.append('<h3>' + data[_i].country + '</h3>')
+		}
+	}
+
+	send_request = function() {
+		name = $("#name").val()
+		return guess_country("/guess/" + (JSON.stringify(name)), disp);
+	}
+
+	$("#guess").click(send_request)
+
+	$('#name').keypress(function (e) {
+  		if (e.which == 13) {
+	    	$('#guess').click();
+	    	return false;
+		}
+	});
+
+	send_request()
+
+}).call(this)
